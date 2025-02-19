20cd66251255a9dc001b9ab78618db9893fc590a
dipalipawshe <pawshedipali93@gmail.com>
2025-02-19 11:41:05 +0530

    presigned url

---
+++ src/presigneds3.ts
@@ -0,0 +1,17 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({ region: "us-east-1" });
+
+async function generatePresignedUrl() {
+  const command = new PutObjectCommand({
+    Bucket: process.env.S3_BUCKET,
+    Key: "data.csv", // Change the file name dynamically if needed
+    ContentType: "csv", // Adjust the content type
+  });
+
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 }); // 1 hour
+  console.log("Pre-signed URL:", url);
+}
+
+generatePresignedUrl();
